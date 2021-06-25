cee9dc0d227a269b230ea8a57ad2ffd1b5f90623
poonam <poonam398@gmail.com>
2021-06-25 18:30:56 -0400

    nav

--- budget-app/src/Components/NavBar.js
+++ budget-app/src/Components/NavBar.js
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav class="navbar  navbar-expand-lg navbar-light ">
+    <nav class="navbar  navbar-expand-lg navbar-light bg-light">
       
-        <NavLink  to="/transactions" class="nav-item nav-link ">Transactions</NavLink>
+        <NavLink  to="/transactions" class="nav-item">Transactions</NavLink>
       
-        <NavLink to="/transactions/new" class="nav-item nav-link">New Transaction</NavLink>
+        <NavLink to="/transactions/new" class="nav-item">New Transaction</NavLink>
     </nav>
   );
 }
