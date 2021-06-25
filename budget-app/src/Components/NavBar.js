61b83f5cea8208997a932e3bdee29d573dcceb17
poonam <poonam398@gmail.com>
2021-06-25 18:32:53 -0400

    navbar a

--- budget-app/src/Components/NavBar.js
+++ budget-app/src/Components/NavBar.js
@@ -4,9 +4,9 @@ export default function NavBar() {
   return (
     <nav class="navbar  navbar-expand-lg navbar-light bg-light">
       
-        <NavLink  to="/transactions" class="nav-item">Transactions</NavLink>
+        <NavLink  to="/transactions" >Transactions</NavLink>
       
-        <NavLink to="/transactions/new" class="nav-item">New Transaction</NavLink>
+        <NavLink to="/transactions/new">New Transaction</NavLink>
     </nav>
   );
 }
